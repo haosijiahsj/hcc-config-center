204cc1148340ad12a59ed90e0023a122aaba2a3c
hushengjun <1017547773@qq.com>
2022-10-28 14:35:08 +0800

    优化netty客户端重启成功后，立即刷新动态配置

--- hcc-config-center-client/src/main/java/com/hcc/config/center/client/netty/ConfigCenterClient.java
+++ hcc-config-center-client/src/main/java/com/hcc/config/center/client/netty/ConfigCenterClient.java
@@ -2,6 +2,8 @@ package com.hcc.config.center.client.netty;
 
 import com.hcc.config.center.client.ProcessFailedCallBack;
 import com.hcc.config.center.client.context.ConfigContext;
+import com.hcc.config.center.client.entity.AppConfigInfo;
+import com.hcc.config.center.client.entity.MsgInfo;
 import com.hcc.config.center.client.processor.ConfigCenterMsgProcessor;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
@@ -13,6 +15,11 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 客户端，接受服务端消息，动态刷新值
@@ -29,6 +36,7 @@ public class ConfigCenterClient {
     private String host;
     private int port;
 
+    private ConfigCenterMsgProcessor configCenterMsgProcessor;
 
     /**
      * 启动客户端
@@ -63,7 +71,7 @@ public class ConfigCenterClient {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
                             ChannelPipeline pipeline = socketChannel.pipeline();
-                            ConfigCenterMsgProcessor configCenterMsgProcessor = new ConfigCenterMsgProcessor(configContext, callBack);
+                            configCenterMsgProcessor = new ConfigCenterMsgProcessor(configContext, callBack);
                             pipeline.addLast(new ConfigCenterIdleStateHandler(configContext, that));
                             pipeline.addLast(new ConfigCenterClientHandler(configCenterMsgProcessor));
                         }
@@ -83,8 +91,47 @@ public class ConfigCenterClient {
         return future -> {
             if (future.isSuccess()) {
                 log.info("动态推送客户端{}成功！", isReconnect ? "重启" : "启动");
+                if (isReconnect) {
+                    this.refreshDynamicConfig();
+                    log.info("重连后刷新动态配置成功！");
+                }
             }
         };
+    }
+
+    /**
+     * 刷新动态配置
+     */
+    private void refreshDynamicConfig() {
+        List<AppConfigInfo> appConfigInfos = configContext.getDynamicConfigFromConfigCenter();
+        if (StringUtils.isEmpty(appConfigInfos)) {
+            return;
+        }
+
+        this.convertToMsgInfo(appConfigInfos).forEach(configCenterMsgProcessor::addMsgToQueue);
+    }
+
+    /**
+     * 转换为可处理的消息
+     * @param dynamicAppConfigInfos
+     * @return
+     */
+    private List<MsgInfo> convertToMsgInfo(List<AppConfigInfo> dynamicAppConfigInfos) {
+        return dynamicAppConfigInfos.stream()
+                .map(c -> {
+                    MsgInfo msgInfo = new MsgInfo();
+                    BeanUtils.copyProperties(c, msgInfo);
+                    if (c.getVersion() == 0) {
+                        msgInfo.setForceUpdate(true);
+                        msgInfo.setMsgType(MsgInfo.MsgType.CONFIG_DELETE.name());
+                    } else {
+                        msgInfo.setForceUpdate(false);
+                        msgInfo.setMsgType(MsgInfo.MsgType.CONFIG_UPDATE.name());
+                    }
+
+                    return msgInfo;
+                })
+                .collect(Collectors.toList());
     }
 
 }
