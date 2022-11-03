4bab09e43b3489b354a07de9dcaa98fb472637e2
hushengjun <1017547773@qq.com>
2022-11-03 15:13:21 +0800

    提供spring的解析支持

---
+++ hcc-config-center-client/src/main/java/com/hcc/config/center/client/spring/ConfigCenterPropertySourcesPlaceholderConfigurer.java
@@ -0,0 +1,34 @@
+package com.hcc.config.center.client.spring;
+
+import com.hcc.config.center.client.context.ConfigContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.util.CollectionUtils;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * 为了spring的Value注解能够获取到值
+ *
+ * @author hushengjun
+ * @date 2022/10/9
+ */
+public class ConfigCenterPropertySourcesPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
+
+    private final ConfigContext configContext;
+
+    public ConfigCenterPropertySourcesPlaceholderConfigurer(ConfigContext configContext) {
+        this.configContext = configContext;
+    }
+
+    @Override
+    protected void loadProperties(Properties props) throws IOException {
+        super.loadProperties(props);
+        Map<String, String> configKeyValueMap = configContext.getConfigKeyValueMap();
+        if (!CollectionUtils.isEmpty(configKeyValueMap)) {
+            props.putAll(configKeyValueMap);
+        }
+    }
+
+}
